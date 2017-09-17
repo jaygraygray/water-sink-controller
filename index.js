770fa65613203ba168d09a631cff47ddf52a1b1b
jaygraygray <jgray@mediasilo.com>
2017-09-17 18:37:32 -0400

    Server functioning; can't get responses to front end but working in postman

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require('express')()
         bodyParser = require('body-parser')
         cors = require('cors')
-        port = process.env.port || 9999
+        port = process.env.port || 6969
         defaultCtrl = require('./controllers/default');
 
 app.use(bodyParser.json());
